1d0aae471427f64aeef41dba3c0c4a2311c15a95
Unknown <andrew.freebairn@csiro.au>
2020-11-13 15:44:05 +1100

    Extend Setup

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 import setuptools
 
-setuptools.setup(name='core',
-      version='0.1.1',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='pybomwater',
+      version='0.0.1',
+      author='Andrew Freebairn',
+      author_email='andrew.freebairn@csiro.au',
       description='A tool for requesting data from BoM Water Data service.',
-      author='C.S.I.R.O.',
-    #   packages = setuptools.find_packages(),
-      packages=['core',
-                'test',
-      ],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/csiro-hydroinformatics/pybomwater",
+      packages=setuptools.find_packages(),
+      # packages=['bom_water',
+      #           'test',
+      # ],
       install_requires=[
             'requests',
             'iso8601',
             'pytz',
-            'json5',
             'xmltodict',
-            'os',
             'pandas'
       ],
-      url='https://github.com/csiro-hydroinformatics/bom_water',
       zip_safe=False)
 
 
