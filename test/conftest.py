29f0f10dd2cfa8736f87c36105c03b10ee1bdeb6
Leighton, Ben (Environment, Clayton) <Ben.Leighton@csiro.au>
2026-07-30 16:05:40 +1000

    load and notebook tests

--- test/conftest.py
+++ test/conftest.py
@@ -21,10 +21,31 @@ LIVE_BOM_TESTS = {
 
 def pytest_collection_modifyitems(config, items):
     run_live = os.environ.get('PYBOMWATER_RUN_LIVE') == '1'
+    run_load = os.environ.get('PYBOMWATER_RUN_LOAD') == '1'
+    run_notebooks = os.environ.get('PYBOMWATER_RUN_NOTEBOOKS') == '1'
     skip_live = pytest.mark.skip(
         reason='live BoM integration test; set PYBOMWATER_RUN_LIVE=1 to run'
     )
+    skip_load = pytest.mark.skip(
+        reason='live BoM load test; set PYBOMWATER_RUN_LOAD=1 to run'
+    )
+    skip_notebooks = pytest.mark.skip(
+        reason=(
+            'live notebook integration test; set '
+            'PYBOMWATER_RUN_NOTEBOOKS=1 to run'
+        )
+    )
     for item in items:
+        if 'notebook_bom' in item.keywords:
+            if not run_notebooks:
+                item.add_marker(skip_notebooks)
+            continue
+
+        if 'load_bom' in item.keywords:
+            if not run_load:
+                item.add_marker(skip_load)
+            continue
+
         if item.name in LIVE_BOM_TESTS:
             item.add_marker(pytest.mark.live_bom)
             if not run_live:
